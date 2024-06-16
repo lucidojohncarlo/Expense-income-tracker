970b1a456de1aba3bc9b735a1491b9c5809fe2a8
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-16 21:40:59 +0800

    16-06-24

--- budget_tracker/income-expense-tracker/Public/db_connect.php
+++ budget_tracker/income-expense-tracker/Public/db_connect.php
@@ -4,7 +4,7 @@ $username = "root"; // Your database username
 $password = ""; // Your database password
 $dbname = "budget_tracker"; // Your database name
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, 3306, null, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
