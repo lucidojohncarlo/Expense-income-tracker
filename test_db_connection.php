50f71f7107c31ac855599f9f9bcd25bba2a9d430
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-17 16:25:50 +0800

    17-06-24

---
+++ test_db_connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = getenv("DB_Server");
+$username = getenv("DB_Username");
+$password = getenv("DB_Password");
+$dbname = getenv("DB_Database");
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
