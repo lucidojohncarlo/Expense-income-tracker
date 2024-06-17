10cd0cd80ab403c3b18d86ce0b671122859f0510
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-17 21:00:22 +0800

    latest

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,14 @@
 <?php
 // Update these values with your actual database credentials
-// $servername = getenv("DB_Server");
-// $username = getenv("DB_Username");
-// $password = getenv("DB_Password");
-// $dbname = getenv("DB_Database");
+$servername = getenv("DB_Server");
+$username = getenv("DB_Username");
+$password = getenv("DB_Password");
+$dbname = getenv("DB_Database");
 
-$servername ="jcserver.mysql.database.azure.com" ;
-$username = "johncarlo";
-$password = "Jhared123";
-$dbname = "budget_tracker";
+// $servername ="jcserver.mysql.database.azure.com" ;
+// $username = "johncarlo";
+// $password = "Jhared123";
+// $dbname = "budget_tracker";
 
 // $servername ="localhost" ;
 // $username = "root";
@@ -16,11 +16,11 @@ $dbname = "budget_tracker";
 // $dbname = "budget_tracker";
 
 // Create connection with SSL
-// $conn = new mysqli($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+$conn = new mysqli($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
 
-$conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, "C:\Users\lucidojm\Downloads", NULL, NULL);
-mysqli_real_connect($conn, $server, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
+// $conn = mysqli_init();
+// mysqli_ssl_set($conn, NULL, NULL, "C:\Users\lucidojm\Downloads", NULL, NULL);
+// mysqli_real_connect($conn, $server, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
 
 
 // $conn = new mysqli($servername, $username, $password, $dbname);
