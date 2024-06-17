d27675d9520a66d9e05f8a407e214a8c07246ada
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-17 16:05:27 +0800

    16-07-24

--- db_connect.php
+++ db_connect.php
@@ -5,9 +5,9 @@
 //$password = getenv("DB_Password");
 //$dbname = getenv("DB_Database");
 
-$servername ="localhost" ;
-$username = "root";
-$password = "";
+$servername ="lucidoserver.mysql.database.azure.com" ;
+$username = "JC";
+$password = "Jhared123";
 $dbname = "budget_tracker";
 
 // Create connection
