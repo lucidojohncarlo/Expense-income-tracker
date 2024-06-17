c32c02aa44db8f7c4a47d126174a7b67a9d7a723
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-17 19:00:29 +0800

    17-06-24

--- db_connect.php
+++ db_connect.php
@@ -5,18 +5,23 @@
 //$password = getenv("DB_Password");
 //$dbname = getenv("DB_Database");
 
-//$servername ="lucidoserver.mysql.database.azure.com" ;
-//$username = "JC";
-//$password = "Jhared123";
-//$dbname = "budget_tracker";
+// $servername ="jcserver.mysql.database.azure.com" ;
+// $username = "johncarlo";
+// $password = "Jhared123";
+// $dbname = "budget_tracker";
 
-$servername ="localhost" ;
-$username = "root";
-$password = "";
-$dbname = "budget_tracker";
+// $servername ="localhost" ;
+// $username = "root";
+// $password = "";
+// $dbname = "budget_tracker";
 
 // Create connection with SSL
 //$conn = new mysqli($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "C:\Users\lucidojm\Downloads", NULL, NULL);
+mysqli_real_connect($conn, "jcserver.mysql.database.azure.com", "johncarlo", "Jhared123", "budget_tracker", 3306, MYSQLI_CLIENT_SSL);
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
