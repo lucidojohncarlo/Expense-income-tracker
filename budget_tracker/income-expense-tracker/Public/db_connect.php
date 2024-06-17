3eaf10b868e2997b27c91a8e4f9adf029fb1d7b5
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-17 12:33:12 +0800

    16-06-24

--- budget_tracker/income-expense-tracker/Public/db_connect.php
+++ budget_tracker/income-expense-tracker/Public/db_connect.php
@@ -1,12 +1,18 @@
 <?php
-$servername = "lucidojohncarlo.mysql.database.azure.com";
-$username = "lucidojohncarlo"; // Your database username
-$password = "Jhared123"; // Your database password
-$dbname = "budget_tracker"; // Your database name
+$servername = getenv('DB_SERVER');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+$ssl_ca = '/D:\DOWNLOADS/DigiCertGlobalRootCA.crt.pem';
 
-$conn = new mysqli($servername, $username, $password, $dbname, 3306, null, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+$conn = new mysqli($servername, $username, $password, $dbname, 3306, null, MYSQLI_CLIENT_SSL);
+
+// Enable SSL
+$conn->ssl_set(NULL, NULL, $ssl_ca, NULL, NULL);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+echo "Connected successfully";
 ?>
