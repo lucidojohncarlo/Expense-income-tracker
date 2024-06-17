b78618c85800993444be5f916baf87eb170ef8cc
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-17 21:07:18 +0800

    a

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,10 @@
 <?php
 // Update these values with your actual database credentials
-$servername = getenv("DB_Server");
-$username = getenv("DB_Username");
-$password = getenv("DB_Password");
-$dbname = getenv("DB_Database");
+$servername = getenv('DB_Server');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+
 
 // $servername ="jcserver.mysql.database.azure.com" ;
 // $username = "johncarlo";
@@ -16,7 +17,10 @@ $dbname = getenv("DB_Database");
 // $dbname = "budget_tracker";
 
 // Create connection with SSL
-$conn = new mysqli($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+// $conn = new mysqli($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // $conn = mysqli_init();
 // mysqli_ssl_set($conn, NULL, NULL, "C:\Users\lucidojm\Downloads", NULL, NULL);
