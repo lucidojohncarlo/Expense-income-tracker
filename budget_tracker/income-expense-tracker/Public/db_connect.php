139d477726641cab0b374e4407bbca3e9b5eb958
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-17 13:11:44 +0800

    16-06-24

--- budget_tracker/income-expense-tracker/Public/db_connect.php
+++ budget_tracker/income-expense-tracker/Public/db_connect.php
@@ -3,12 +3,11 @@ $servername = getenv('DB_SERVER');
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
-$ssl_ca = '/D:\DOWNLOADS/DigiCertGlobalRootCA.crt.pem';
+
 
 $conn = new mysqli($servername, $username, $password, $dbname, 3306, null, MYSQLI_CLIENT_SSL);
 
-// Enable SSL
-$conn->ssl_set(NULL, NULL, $ssl_ca, NULL, NULL);
+
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
