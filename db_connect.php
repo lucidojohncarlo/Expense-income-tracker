d50a8a9f7cd3929bf8f90cc953ffa8b9f8bd15f8
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-17 18:32:41 +0800

    17-06-24

--- db_connect.php
+++ db_connect.php
@@ -1,19 +1,19 @@
 <?php
 // Update these values with your actual database credentials
-$servername = getenv("DB_Server");
-$username = getenv("DB_Username");
-$password = getenv("DB_Password");
-$dbname = getenv("DB_Database");
+// $servername = getenv("DB_Server");
+// $username = getenv("DB_Username");
+// $password = getenv("DB_Password");
+// $dbname = getenv("DB_Database");
 
 //$servername ="lucidoserver.mysql.database.azure.com" ;
 //$username = "JC";
 //$password = "Jhared123";
 //$dbname = "budget_tracker";
 
-//$servername ="localhost" ;
-//$username = "root";
-//$password = "";
-//$dbname = "budget_tracker";
+$servername ="localhost" ;
+$username = "root";
+$password = "";
+$dbname = "budget_tracker";
 
 // Create connection with SSL
 $conn = new mysqli($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
