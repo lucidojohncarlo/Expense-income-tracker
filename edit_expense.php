00fa1f1251d18fba92afb974a34c1be0beb2bf3c
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-18 23:20:45 +0800

    17-06-24

--- edit_expense.php
+++ edit_expense.php
@@ -1,20 +1,20 @@
 <?php
-require 'db_connect.php';
-
-$id = $_POST['id'];
-$title = $_POST['title'];
-$amount = $_POST['amount'];
-
-$stmt = $conn->prepare("UPDATE expenses SET title = ?, amount = ? WHERE id = ?");
-$stmt->bind_param("sdi", $title, $amount, $id);
-$stmt->execute();
-
-$message = "Edited expense ID $id: $title - ₱$amount";
-$conn->query("INSERT INTO audit_trail (message) VALUES ('$message')");
-
-$stmt->close();
-$conn->close();
-
-// Reload data after operation
-require 'load_data.php';
-?>
+//require 'db_connect.php';
+//
+//$id = $_POST['id'];
+//$title = $_POST['title'];
+//$amount = $_POST['amount'];
+//
+//$stmt = $conn->prepare("UPDATE expenses SET title = ?, amount = ? WHERE id = ?");
+//$stmt->bind_param("sdi", $title, $amount, $id);
+//$stmt->execute();
+//
+//$message = "Edited expense ID $id: $title - ₱$amount";
+//$conn->query("INSERT INTO audit_trail (message) VALUES ('$message')");
+//
+//$stmt->close();
+//$conn->close();
+//
+//// Reload data after operation
+//require 'load_data.php';
+//?>
--- reset_data.php
+++ reset_data.php
@@ -1,16 +1,16 @@
 <?php
-require 'db_connect.php';
-
-// Clear all tables
-$conn->query("TRUNCATE TABLE income");
-$conn->query("TRUNCATE TABLE expenses");
-$conn->query("TRUNCATE TABLE audit_trail");
-
-$message = "Reset all data";
-$conn->query("INSERT INTO audit_trail (message) VALUES ('$message')");
-
-$conn->close();
-
-// Reload data after operation
-require 'load_data.php';
-?>
+//require 'db_connect.php';
+//
+//// Clear all tables
+//$conn->query("TRUNCATE TABLE income");
+//$conn->query("TRUNCATE TABLE expenses");
+//$conn->query("TRUNCATE TABLE audit_trail");
+//
+//$message = "Reset all data";
+//$conn->query("INSERT INTO audit_trail (message) VALUES ('$message')");
+//
+//$conn->close();
+//
+//// Reload data after operation
+//require 'load_data.php';
+//?>
